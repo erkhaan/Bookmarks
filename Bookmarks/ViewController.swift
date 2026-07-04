7b61b60853dd39ed9fea46c9e9b3380bd97652af
erkhaan <notykt@gmail.com>
2026-07-04 21:43:07 +0500

    Initial commit

---
+++ Bookmarks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bookmarks
+//
+//  Created by Erkhaan  on 04.07.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
